e7c2ecc1667f1cc87081083c086dd2daa5e2203b
Puthasan (Tig) <tiggersam17@gmail.com>
2023-11-20 13:21:09 -0500

    new push

--- Corhort107/javascript/arrays/index.js
+++ Corhort107/javascript/arrays/index.js
@@ -1,48 +1,83 @@
 
 
 //* Array Literal
-const movies = ['Step Brothers', '21 Jumpstreet', 'Wolf of Wallstreet'];
-// this will print out 'Wolf of Wallstreet'
-// console.log(movies[2]);
+// const movies = ['Step Brothers', '21 Jumpstreet', 'Wolf of Wallstreet'];
+// // this will print out 'Wolf of Wallstreet'
+// // console.log(movies[2]);
 
 
-//? OVERWRITE A VALUE
-movies[1] = 'Batman';
+// //? OVERWRITE A VALUE
+// movies[1] = 'Batman';
 
-console.log(movies.length);
-console.log(movies);
+// console.log(movies.length);
+// console.log(movies);
 
-//
-for (let i = 0; i < movies.length; i++) {
-  console.log(movies[i]);
+// //
+// for (let i = 0; i < movies.length; i++) {
+//   console.log(movies[i]);
 
-}
+// }
 
-let yourMovies = ['Caddyshack', 'Interstellar', 'Scarface'];
-let myMovies = ['Batman', 'The Count of Monte Cristo', 'Shrek'];
+// let yourMovies = ['Caddyshack', 'Interstellar', 'Scarface'];
+// let myMovies = ['Batman', 'The Count of Monte Cristo', 'Shrek'];
 
-const ourMovies = yourMovies.concat(myMovies, movies);
+// const ourMovies = yourMovies.concat(myMovies, movies);
 
-//? concat
-const copyOfMovies = movies.concat();
+// //? concat
+// const copyOfMovies = movies.concat();
 
-//? push a new array
-copyOfMovies.push('Toy Story');
+// //? push a new array
+// copyOfMovies.push('Toy Story');
 
 
-copyOfMovies.push('Batman 2');
-console.log(copyOfMovies);
+// copyOfMovies.push('Batman 2');
+// console.log(copyOfMovies);
 
-// removing the last element in the array
-const lastEle = copyOfMovies.pop();
+// // removing the last element in the array
+// const lastEle = copyOfMovies.pop();
 
-console.log(copyOfMovies);
+// console.log(copyOfMovies);
 
-console.log(`The movie ${lastEle} was removed.`);
+// console.log(`The movie ${lastEle} was removed.`);
 
-const moviesName = copyOfMovies.join(' - ')
+// const moviesName = copyOfMovies.join(' - ')
 
-console.log(moviesName);
+// console.log(moviesName);
 
-movies.push('Trading Places, Antitrust');
-movies.unshift('Star Wars');
+// movies.push('Trading Places, Antitrust');
+// movies.unshift('Star Wars');
+
+/ x|o|x
+// x|x|o
+// x|o|o
+const board = [
+    ['x', 'o', 'x'], // row 1
+    ['x', 'x', 'o'], // row 2
+    ['x', 'o', 'o'] // row 3
+];
+// breakoutRooms[row][column]
+console.log(breakoutRooms[2][1]);
+console.log("========================");
+let numArray = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [
+        [7, 8, 9],
+        [10, 11, 12],
+        [13, 14, 15]
+    ],
+];
+console.log( numArray);
+
+const twoDArray = numArray.flat(2);
+
+console.log(twoDArray);
+
+console.log( numArray);
+
+let numArray = [4, 8, 1, 0, -14, 22, 77, 1388, 20];
+
+const sortedArray = numArray2.toSorted((a, b) => a - b);
+console.log(sortedArray);
+
+console.log(numArray2);
--- Corhort107/javascript/loops/index.js
+++ Corhort107/javascript/loops/index.js
@@ -122,3 +122,59 @@
 // }
 
 
+//? fizzbuzz
+
+// for (let i = 1; i <= 100; i++) {
+//   if (i % 3 == 0 && i % 5 == 0) {
+//     console.log("Fizz Buzz");
+
+//   } else if (i % 3 == 0) {
+//     console.log("Fizz");
+
+//   } else if (i % 5 == 0) {
+//     console.log("Buzz");
+    
+//   } else {
+//     console.log(i);
+//   }
+// }
+
+//? Prime Time
+
+// for (let n = 5; n <= 12; n++) {
+//   let isPrime = true;
+
+//   for (let i = 2; i < n; i++) {
+//       // i = 2, 3, 4
+//       if (n % i == 0) {
+//           isPrime = false; // not a prime number
+//           break;
+//       }
+//   }
+
+//   if (isPrime) {
+//       console.log(`${n} is a prime number.`);
+//   }
+
+// }
+
+//? Feeling Loopy
+
+const csvString = "Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232";
+
+function parseCSV(csv) {
+  // Split the CSV string into rows based on the newline character
+  const rows = csv.split('\n');
+
+  // Loop through each row
+  for (let i = 0; i < rows.length; i++) {
+    // Split the row into cells based on the comma as a delimiter
+    const cells = rows[i].split(',');
+
+    // Log each cell for the current row
+    console.log(cells[0], cells[1], cells[2], cells[3]);
+  }
+}
+
+// Call the function with the provided CSV string
+parseCSV(csvString);
