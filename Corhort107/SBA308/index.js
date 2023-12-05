dd37002066187736ebe01fcef1de2762f1374f60
Puthasan (Tig) <tiggersam17@gmail.com>
2023-12-04 19:58:37 -0500

    learning how to debit mapping

--- Corhort107/SBA308/index.js
+++ Corhort107/SBA308/index.js
@@ -1,111 +1,73 @@
 function getLearnerData(courseInfo, assignmentGroup, learnerSubmissions) {
-  // Validate course ID
-  if (assignmentGroup.course_id !== courseInfo.id) {
-    throw new Error('Invalid input: AssignmentGroup does not belong to the specified course.');
-  }
-  
-  // Validate and process assignment data
-  const assignmentData = processAssignments(assignmentGroup);
-  
-  // Validate and process learner submissions
-  const learnerData = processLearnerSubmissions(learnerSubmissions, assignmentData);
-  
-  // Calculate weighted average and format result
-  const result = calculateWeightedAverage(learnerData);
-  
-  return result;
-}
-
-function processAssignments(assignmentGroup) {
-  const assignmentData = {};
-  
-  for (const assignmentInfo of assignmentGroup.assignments) {
-    // Validate points_possible
-    if (assignmentInfo.points_possible <= 0) {
-      // Handle error gracefully
-      console.error(`Invalid data: points_possible is zero or negative for assignment ${assignmentInfo.id}`);
-      continue;
-    }
-    
-    // Check if assignment is overdue
-    const now = new Date();
-    const dueDate = new Date(assignmentInfo.due_at);
-    if (now > dueDate) {
-      // Deduct 10 percent for late submission
-      assignmentInfo.points_possible *= 0.9;
-    }
-    
-    assignmentData[assignmentInfo.id] = assignmentInfo;
-  }
-  
-  return assignmentData;
-}
-
-function processLearnerSubmissions(learnerSubmissions, assignmentData) {
-  const learnerData = {};
-  
-  for (const submission of learnerSubmissions) {
-    const assignmentInfo = assignmentData[submission.assignment_id];
-    
-    if (assignmentInfo) {
-      const weightedScore = (submission.submission.score / assignmentInfo.points_possible) * 100;
-      
-      // If assignment is not due, do not include it
-      if (new Date() > new Date(assignmentInfo.due_at)) {
-        learnerData[assignmentInfo.id] = weightedScore;
-      }
-    } else {
-      // Handle error gracefully
-      console.error(`Invalid data: Submission for non-existent assignment ${submission.assignment_id}`);
-    }
-  }
-  
-  return learnerData;
-}
-
-function calculateWeightedAverage(learnerData) {
   const result = [];
-  
-  for (const learnerId in learnerData) {
-    if (learnerData.hasOwnProperty(learnerId)) {
-      const learnerTotal = Object.values(learnerData[learnerId]).reduce((acc, score) => acc + score, 0);
-      const totalWeight = Object.keys(learnerData[learnerId]).length;
-      
-      // Calculate weighted average
-      const average = learnerTotal / totalWeight;
-      
-      // Format result
-      result.push({
-        id: parseInt(learnerId),
-        avg: average,
-        ...learnerData[learnerId],
+
+  // Create a map to store scores and possible scores for each assignment
+  const assignmentInfo = new Map();
+  assignmentGroup.assignments.forEach((assignment) => {
+    assignmentInfo.set(assignment.id, {
+      points_possible: assignment.points_possible,
+      due_at: new Date(assignment.due_at).getTime(),
+    });
+  });
+
+  // Process learner submissions
+  const learnerData = new Map();
+  learnerSubmissions.forEach((submission) => {
+    const { learner_id, assignment_id, submission: { score, submitted_at } } = submission;
+
+    if (!learnerData.has(learner_id)) {
+      learnerData.set(learner_id, {
+        id: learner_id,
+        totalScore: 0,
+        totalPossibleScore: 0,
+        scores: {},
       });
     }
-  }
-  
+
+    const learner = learnerData.get(learner_id);
+    const assignment = assignmentInfo.get(assignment_id);
+    const assignmentKey = assignment_id.toString();
+
+    // Calculate late penalty (if submission is late)
+    const submittedTime = new Date(submitted_at).getTime();
+    const dueTime = assignment.due_at;
+    const isLate = submittedTime > dueTime;
+    const latePenalty = isLate ? (submittedTime - dueTime) / (24 * 60 * 60 * 1000) * 15 : 0;
+
+    // Update learner data
+    learner.totalScore += score - latePenalty;
+    learner.totalPossibleScore += assignment.points_possible;
+    learner.scores[assignmentKey] = score / assignment.points_possible;
+
+    // Update late key
+    if (isLate) {
+      learner[assignmentKey + '_late'] = (score - latePenalty) / assignment.points_possible;
+    }
+  });
+
+  // Calculate averages and format the result
+  learnerData.forEach((learner) => {
+    const avg = learner.totalScore / learner.totalPossibleScore;
+    const formattedLearner = {
+      id: learner.id,
+      avg: (Math.round(avg * 100) / 100) * 100 + '%', 
+    };
+
+    // Add individual assignment scores
+    for (const [assignmentKey, assignmentScore] of Object.entries(learner.scores)) {
+      formattedLearner[assignmentKey] = Math.round(assignmentScore * 1000) / 1000; // Round to 3 decimal places
+    }
+
+    // Add late scores
+    for (const [assignmentKey, lateScore] of Object.entries(learner).filter(([key]) => key.endsWith('_late'))) {
+      formattedLearner[assignmentKey] = Math.round(lateScore * 1000) / 1000; // Round to 3 decimal places
+    }
+
+    result.push(formattedLearner);
+  });
+
   return result;
 }
-
-// Example usage:
-const courseInfo = { id: 1, name: 'Course 1' };
-const assignmentGroup = {
-  id: 1,
-  name: 'Assignment Group 1',
-  course_id: 1,
-  group_weight: 10,
-  assignments: [
-    { id: 101, name: 'Assignment 1', due_at: '2023-12-01', points_possible: 100 },
-    { id: 102, name: 'Assignment 2', due_at: '2023-12-10', points_possible: 150 },
-  ],
-};
-
-const learnerSubmissions = [
-  { learner_id: 1, assignment_id: 101, submission: { submitted_at: '2023-11-28', score: 90 } },
-  { learner_id: 1, assignment_id: 102, submission: { submitted_at: '2023-12-15', score: 130 } },
-];
-
-console.log(getLearnerData(courseInfo, assignmentGroup, learnerSubmissions));
-// The provided course information.
 const CourseInfo = {
   id: 451,
   name: "Introduction to JavaScript"
@@ -183,27 +145,5 @@ const LearnerSubmissions = [
   }
 ];
 
-function getLearnerData(course, ag, submissions) {
-  // here, we would process this data to achieve the desired result.
-  const result = [
-    {
-      id: 125,
-      avg: 0.985, // (47 + 150) / (50 + 150)
-      1: 0.94, // 47 / 50
-      2: 1.0 // 150 / 150
-    },
-    {
-      id: 132,
-      avg: 0.82, // (39 + 125) / (50 + 150)
-      1: 0.78, // 39 / 50
-      2: 0.833 // late: (140 - 15) / 150
-    }
-  ];
-
-  return result;
-}
-
 const result = getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions);
-
 console.log(result);
-console.log(getLearnerData(courseInfo, assignmentGroup));
